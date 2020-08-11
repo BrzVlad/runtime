6cbd5c70ce08f8c33fc1d4591f32db098fa922e7
Vlad Brezae <brezaevlad@gmail.com>
2020-08-11 12:31:55 +0300

    [interp] Add configuration option to disable stack scan

--- src/mono/mono/metadata/sgen-mono.c
+++ src/mono/mono/metadata/sgen-mono.c
@@ -2361,6 +2361,7 @@ sgen_client_scan_thread_data (void *start_nursery, void *end_nursery, gboolean p
 
 		aligned_stack_start = get_aligned_stack_start (info);
 		g_assert (info->client_info.suspend_done);
+#ifndef INTERP_NO_STACK_SCAN
 		SGEN_LOG (3, "Scanning thread %p, range: %p-%p, size: %" G_GSIZE_FORMAT "d, pinned=%" G_GSIZE_FORMAT "d", info, info->client_info.stack_start, info->client_info.info.stack_end, (char*)info->client_info.info.stack_end - (char*)info->client_info.stack_start, sgen_get_pinned_count ());
 		if (mono_gc_get_gc_callbacks ()->thread_mark_func && !conservative_stack_mark) {
 			mono_gc_get_gc_callbacks ()->thread_mark_func (info->client_info.runtime_data, (guint8 *)aligned_stack_start, (guint8 *)info->client_info.info.stack_end, precise, &ctx);
@@ -2388,6 +2389,7 @@ sgen_client_scan_thread_data (void *start_nursery, void *end_nursery, gboolean p
 				}
 			}
 		}
+#endif
 		if (gc_callbacks.interp_mark_func) {
 			PinHandleStackInteriorPtrData ud;
 			memset (&ud, 0, sizeof (ud));
