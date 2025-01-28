2293a92bdf47bb903e1cfc75d8a26849db9e36ee
Vlad Brezae <brezaevlad@gmail.com>
2025-01-28 10:34:38 +0200

    Add basic compilation and execution to the clr interpreter

--- src/coreclr/interpreter/interpreter.h
+++ src/coreclr/interpreter/interpreter.h
@@ -1,3 +1,28 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+#include <stdint.h>
+#include <wchar.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <string.h>
+#include <float.h>
+#include <cstdlib>
+#include <cmath>
+#include <assert.h>
+#ifdef HOST_WINDOWS
+#include <malloc.h>
+#endif
+#include <algorithm>
+
+#include "corhdr.h"
+#include "corjit.h"
+
+#include "compiler.h"
+
+#define ALIGN_UP_TO(val,align) ((((size_t)val) + (size_t)((align) - 1)) & (~((size_t)(align - 1))))
+#define INTERP_STACK_SLOT_SIZE 8    // Alignment of each var offset on the interpreter stack
+#define INTERP_STACK_ALIGNMENT 16   // Alignment of interpreter stack at the start of a frame
+#define INTERP_STACK_SIZE 1024*1024
