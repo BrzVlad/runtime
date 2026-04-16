9a5d505c98668ff4558a3ad8ce1c793851f5f8d1
Vlad Brezae <brezaevlad@gmail.com>
2026-04-16 17:55:34 +0300

    Update src/coreclr/tools/aot/ILCompiler.ReadyToRun/JitInterface/CorInfoImpl.ReadyToRun.cs
    Co-authored-by: Jan Kotas <jkotas@microsoft.com>

--- src/coreclr/tools/aot/ILCompiler.ReadyToRun/JitInterface/CorInfoImpl.ReadyToRun.cs
+++ src/coreclr/tools/aot/ILCompiler.ReadyToRun/JitInterface/CorInfoImpl.ReadyToRun.cs
@@ -2008,7 +2008,9 @@ namespace Internal.JitInterface
                 MethodDesc directMethod = constrainedType.TryResolveConstraintMethodApprox(exactType, originalMethod, out forceUseRuntimeLookup, ref dimResolution);
                 if (isStaticVirtual && directMethod != null &&
                         !_compilation.NodeFactory.CompilationModuleGroup.VersionsWithMethodBody(directMethod))
+                {
                     directMethod = null;
+                }
 
                 if (directMethod != null)
                 {
